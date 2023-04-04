020169fed69eb1870e274586086cd0f75d807ab7
Ian Gow <iandgow@gmail.com>
2023-04-05 09:32:02 +1000

    Tweaked CREATE TABLE earthquakes.

--- Chapter 6: Anomaly Detection/create_earthquakes_table.sql
+++ Chapter 6: Anomaly Detection/create_earthquakes_table.sql
@@ -1,5 +1,5 @@
-DROP table if exists public.earthquakes;
-CREATE table public.earthquakes
+DROP table if exists sql_book.earthquakes;
+CREATE table sql_book.earthquakes
 (
 time timestamp with time zone
 ,latitude decimal
@@ -29,32 +29,32 @@ time timestamp with time zone
 SET timezone TO 'UTC';
 -- replace localpath with the directory where you saved the csv files. The data set is large - the code will run if you load one or a few files, but the results will differ.
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes1.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes1.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes2.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes2.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes3.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes3.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes4.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes4.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes5.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes5.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes6.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes6.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes7.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes7.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes8.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes8.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes9.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes9.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes10.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes10.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes11.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes11.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes12.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes12.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes13.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes13.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes14.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes14.csv' DELIMITER ',' CSV HEADER;
 
-COPY public.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes15.csv' DELIMITER ',' CSV HEADER;
+COPY sql_book.earthquakes FROM '/Users/igow/git/sql_book/Chapter 6: Anomaly Detection/earthquakes15.csv' DELIMITER ',' CSV HEADER;
