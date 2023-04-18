e5ded302346a4ad6850c8d7ef6504e560787aef6
Ian Gow <iandgow@gmail.com>
2023-04-18 20:30:47 +1000

    Cleaned up import_earthquakes.R.

--- import_earthquakes.R
+++ import_earthquakes.R
@@ -1,22 +1,23 @@
 library(DBI)
 library(tidyverse)
 
-files <- list.files("Chapter 6: Anomaly Detection", 
-                    pattern = "*.csv",
+files <- list.files("data", 
+                    pattern = "earthquakes*",
                     full.names = TRUE)
 
-pg <- dbConnect(RPostgres::Postgres())
-rs <- dbExecute(pg, "SET search_path TO sql_book")
-rs <- dbExecute(pg, "DROP TABLE IF EXISTS earthquakes_new")
-rs <- dbDisconnect(pg)
+db <- dbConnect(duckdb::duckdb(), 
+                dbdir = "earthquakes.duckdb",
+                read_only = FALSE)
+rs <- dbExecute(db, "DROP TABLE IF EXISTS earthquakes")
+rs <- dbDisconnect(db, shutdown = TRUE)
 
 import_file <- function(file) {
   df <- read_csv(file, locale = locale(tz = "UTC"),
                  col_types = "TnnnncnnnnccTccnnnccc")  
-  pg <- dbConnect(RPostgres::Postgres())
-  rs <- dbExecute(pg, "SET search_path TO sql_book")
-  dbWriteTable(pg, "earthquakes_new", df, append = TRUE)
-  dbDisconnect(pg)
+  db <- dbConnect(duckdb::duckdb(), dbdir = "data/earthquakes.duckdb",
+                  read_only = FALSE)
+  dbWriteTable(db, "earthquakes", df, append = TRUE)
+  dbDisconnect(db, shutdown=TRUE)
 }
 
 res <- unlist(lapply(files, import_file)) 
